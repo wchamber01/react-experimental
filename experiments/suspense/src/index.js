e8c37b03b961e553bc01b173a2ef80fccd1f2ca6
samsisle <samko9522@gmail.com>
2019-10-28 22:54:22 -0700

    🌳 Switch to createRoot and <StrictMode>

--- experiments/suspense/src/index.js
+++ experiments/suspense/src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { StrictMode } from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
@@ -6,7 +6,11 @@ import * as serviceWorker from "./serviceWorker";
 
 import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
