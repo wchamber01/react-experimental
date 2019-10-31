4c682e009c6bd88a5d0e7d3aedd66e1ea651e942
samsisle <samko9522@gmail.com>
2019-10-30 23:25:05 -0700

    🌲 Delete default css

--- experiments/suspense/src/App.js
+++ experiments/suspense/src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
 
-import './css/App.css';
-
 export default function App() {
   return (
-    <div className="App">
-      <div>bruhh!!</div>
+    <div>
+      <div>Samm</div>
     </div>
   );
 }
