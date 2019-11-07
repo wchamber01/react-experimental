885c13e40162bf9fe87695b1a3f02d4a629b526a
samsisle <samko9522@gmail.com>
2019-11-07 01:27:11 -0800

    Add fetch utility

--- experiments/films/src/lib/fetch.js
+++ experiments/films/src/lib/fetch.js
@@ -1,6 +1,13 @@
-import fetch from 'isomorphic-unfetch';
+import fetch from 'unfetch';
 
-export default async function(...args) {
-  const res = await fetch(...args);
+export default async function(url) {
+  const res = await fetch(url, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${process.env.REACT_APP_TMDB_ACCESS_TOKEN}`,
+      'Content-Type': 'application/json;charset=utf-8'
+    }
+  });
+
   return await res.json();
 }
