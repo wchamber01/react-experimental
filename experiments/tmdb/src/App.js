aaad0f53cefee032e5ee3d9b9727c0dd0b7f4c1e
samsisle <samko9522@gmail.com>
2019-11-29 23:30:34 -0800

    Update suspense fallback & add new movie route

--- experiments/tmdb/src/App.js
+++ experiments/tmdb/src/App.js
@@ -2,15 +2,18 @@ import React, { Suspense } from 'react';
 import { Router } from '@reach/router';
 
 import Header from './components/Header';
+import Spinner from './components/Spinner';
 import NowPlaying from './components/movies/NowPlaying';
+import Movie from './components/movies/Movie';
 
 export default function App() {
   return (
     <>
       <Header />
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense fallback={<Spinner />}>
         <Router>
           <NowPlaying path="/" />
+          <Movie path=":movieTitle/:movieId" />
         </Router>
       </Suspense>
     </>
