3b697f8e0aff2f3eb751b8f48a5a73c9b989fa22
samsisle <samko9522@gmail.com>
2019-11-29 23:31:20 -0800

    Add Link inside Poster component

--- experiments/tmdb/src/components/movies/Poster.js
+++ experiments/tmdb/src/components/movies/Poster.js
@@ -1,15 +1,20 @@
 import React from 'react';
+import { Link } from '@reach/router';
 
 import css from '../../css/Poster.module.css';
 
 export default function Poster(props) {
+  const formatted_title = props.title.toLowerCase().replace(/\s/g, '-');
+
   return (
     <li className={css.container}>
-      <img
-        className={css.poster}
-        src={`https://image.tmdb.org/t/p/w500${props.posterPath}`}
-        alt={props.title}
-      />
+      <Link to={`/${formatted_title}/${props.id}`}>
+        <img
+          className={css.poster}
+          src={`https://image.tmdb.org/t/p/w342${props.posterPath}`}
+          alt={props.title}
+        />
+      </Link>
     </li>
   );
 }
