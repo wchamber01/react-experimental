681c7eae92b8b210031cc4d9cf741056eec455fa
samsisle <samko9522@gmail.com>
2019-12-04 01:01:26 -0800

    Add prefetching methods to Poster component

--- experiments/tmdb/src/components/movies/Poster.js
+++ experiments/tmdb/src/components/movies/Poster.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import { Link } from '@reach/router';
+import { mutate } from 'swr';
+
+import fetch from '../../lib/fetch';
 
 import css from '../../css/Poster.module.css';
 
 export default function Poster(props) {
   const formatted_title = props.title.toLowerCase().replace(/\s/g, '-');
 
+  function handleMouseEnter() {}
+
+  function handleMouseLeave() {}
+
+  async function prefetch() {
+    const data = await fetch(
+      `https://api.themoviedb.org/3/movie/${props.id}?page=1&language=en-US`
+    );
+    mutate(
+      `https://api.themoviedb.org/3/movie/${props.id}?page=1&language=en-US`,
+      data,
+      false
+    );
+  }
+
   return (
-    <li className={css.container}>
+    <li className={css.container} onMouseEnter={handleMouseEnter} onMouseLeave>
       <Link to={`/${formatted_title}/${props.id}`}>
         <img
           className={css.poster}
