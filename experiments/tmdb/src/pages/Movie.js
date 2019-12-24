e53b5b2917c3093fb216476f6061a68f926c9142
samsisle <samko9522@gmail.com>
2019-12-24 01:01:03 -0800

    Move Movie component to page dir

---
+++ experiments/tmdb/src/pages/Movie.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { useParams, Route } from 'react-router-dom';
+import useSwr from 'swr';
+
+import css from '../css/Movie.module.css';
+
+function Movie() {
+  const { movieId } = useParams();
+
+  const {
+    data
+  } = useSwr(
+    `https://api.themoviedb.org/3/movie/${movieId}?page=1&language=en-US`,
+    { suspense: true }
+  );
+
+  return (
+    <div className={css.container}>
+      <img
+        className={css.backdrop}
+        src={`https://image.tmdb.org/t/p/original${data.backdrop_path}`}
+        alt={data.title}
+      />
+      <h2>{data.title}</h2>
+      <p>{data.overview}</p>
+    </div>
+  );
+}
+
+/**
+ * Necessary in order to have nested route
+ * props working with react-router-dom.
+ */
+export default function MovieRoute() {
+  const { movieTitle } = useParams();
+
+  return (
+    <Route path={`/${movieTitle}/:movieId`}>
+      <Movie />
+    </Route>
+  );
+}
