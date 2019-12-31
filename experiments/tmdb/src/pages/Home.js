83068370af0e97aba9a6a6d619e93f4a987bbf2c
samsisle <samko9522@gmail.com>
2019-12-31 15:26:47 -0800

    Remove useWindowWidth hook from Home page

--- experiments/tmdb/src/pages/Home.js
+++ experiments/tmdb/src/pages/Home.js
@@ -2,16 +2,12 @@ import React from 'react';
 
 import NowPlaying from '../components/movies/NowPlaying';
 
-import useWindowWidth from '../lib/useWindowWidth';
-
 import css from '../css/Home.module.css';
 
 export default function Home() {
-  const width = useWindowWidth();
-
   return (
     <div className={css.container}>
-      <NowPlaying width={width} />
+      <NowPlaying />
     </div>
   );
 }
