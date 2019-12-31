f806a550ec05f408602f8fdfd738c97606cad816
samsisle <samko9522@gmail.com>
2019-12-30 22:28:49 -0800

    Add useWindowWidth custom hook to Home page

--- experiments/tmdb/src/pages/Home.js
+++ experiments/tmdb/src/pages/Home.js
@@ -2,12 +2,16 @@ import React from 'react';
 
 import NowPlaying from '../components/movies/NowPlaying';
 
+import useWindowWidth from '../lib/useWindowWidth';
+
 import css from '../css/Home.module.css';
 
 export default function Home() {
+  const width = useWindowWidth();
+
   return (
     <div className={css.container}>
-      <NowPlaying />
+      <NowPlaying width={width} />
     </div>
   );
 }
