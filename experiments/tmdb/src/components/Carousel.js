d3e873513fe8238be7bd3a49a70b7f793fd57a2f
samsisle <samko9522@gmail.com>
2019-12-31 18:14:51 -0800

    Update Carousel component (unfinished)

--- experiments/tmdb/src/components/Carousel.js
+++ experiments/tmdb/src/components/Carousel.js
@@ -9,11 +9,10 @@ function Button(props) {
   const buttonEl = useRef(null);
 
   function scroll() {
-    console.log(props.listRef);
     const sW = props.listRef.current.scrollWidth;
     const childrenCount = props.listRef.current.childElementCount;
     const move =
-      (sW / (childrenCount / 2)) * (props.scroll === 'left' ? -1 : 1);
+      (sW / (childrenCount / 2)) * (props.direction === 'L' ? -1 : 1);
 
     props.listRef.current.scrollBy({
       top: 0,
@@ -33,12 +32,14 @@ function Button(props) {
       onClick={scroll}
       onPointerUp={blur}
       style={
-        props.scroll === 'left' ? { left: '-1.6rem' } : { right: '-1.6rem' }
+        props.direction === 'L' ? { left: '-1.6rem' } : { right: '-1.6rem' }
       }
     >
       <img
-        src={props.scroll === 'left' ? left : right}
-        alt={`${props.scroll} carousel scroll button`}
+        src={props.direction === 'L' ? left : right}
+        alt={`${
+          props.direction === 'L' ? 'left' : 'right'
+        } carousel scroll button`}
       />
     </button>
   );
@@ -47,18 +48,22 @@ function Button(props) {
 export default function Carousel(props) {
   const listEl = useRef(null);
 
+  // check if device is mobile
   function checkMobile() {
-    if (window.matchMedia('(pointer: coarse)').matches) return false;
-    else return true;
+    if (window.matchMedia('(pointer: coarse)').matches) {
+      return false;
+    } else {
+      return true;
+    }
   }
 
   return (
     <div className={css.container}>
-      {checkMobile() && <Button scroll="left" listRef={listEl} />}
+      {checkMobile() && <Button direction="L" listRef={listEl} />}
       <ul ref={listEl} className={css.list}>
         {props.children}
       </ul>
-      {checkMobile() && <Button scroll="right" listRef={listEl} />}
+      {checkMobile() && <Button direction="R" listRef={listEl} />}
     </div>
   );
 }
