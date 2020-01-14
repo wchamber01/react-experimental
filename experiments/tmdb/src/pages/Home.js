d488fcaa448950ff0371fa8f59d0d0a411ae815d
samsisle <samko9522@gmail.com>
2020-01-13 17:39:39 -0800

    Import Upcoming component

--- experiments/tmdb/src/pages/Home.js
+++ experiments/tmdb/src/pages/Home.js
@@ -3,11 +3,13 @@ import React from 'react';
 import NowPlaying from '../components/movies/NowPlaying';
 
 import css from '../css/Home.module.css';
+import Upcoming from '../components/movies/Upcoming';
 
 export default function Home() {
   return (
     <div className={css.container}>
       <NowPlaying />
+      <Upcoming />
     </div>
   );
 }
