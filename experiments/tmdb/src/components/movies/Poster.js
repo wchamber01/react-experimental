7ee6ed345828ae63d883a66e79bfe6a6efe30742
samsisle <samko9522@gmail.com>
2020-01-13 18:31:34 -0800

    Add formatTitle helper function

--- experiments/tmdb/src/components/movies/Poster.js
+++ experiments/tmdb/src/components/movies/Poster.js
@@ -4,11 +4,21 @@ import { Link } from 'react-router-dom';
 import css from '../../css/Poster.module.css';
 
 export default function Poster(props) {
-  const formatted_title = props.title.toLowerCase().replace(/\s/g, '-');
+  function formatTitle(s) {
+    const formattedTitle = s.toLowerCase().replace(/\W+/g, '-');
+
+    // Some movie titles end in parentheses. For example,
+    // Birds of Prey (And the Fantabulous Emancipation of One Harley Quinn)
+    if (formattedTitle.charAt(formattedTitle - 1) === '-') {
+      return formattedTitle.slice(0, -1);
+    } else {
+      return formattedTitle;
+    }
+  }
 
   return (
     <li className={css.container}>
-      <Link to={`/${formatted_title}/${props.id}`}>
+      <Link to={`/${formatTitle(props.title)}/${props.id}`}>
         <img
           className={css.poster}
           src={`https://image.tmdb.org/t/p/w342${props.posterPath}`}
