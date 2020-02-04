d96d23c602dc72aff23e773570949077e81b4cfe
samsisle <samko9522@gmail.com>
2020-02-04 02:46:47 -0800

    Switch from - to _

--- experiments/tmdb/src/components/movies/Poster.js
+++ experiments/tmdb/src/components/movies/Poster.js
@@ -13,10 +13,10 @@ export default function Poster(props) {
   }
 
   function formatTitle(s) {
-    const formattedTitle = s.toLowerCase().replace(/\W+/g, '-');
+    const formattedTitle = s.toLowerCase().replace(/\W+/g, '_');
     // Some movie titles end in parentheses. For example,
     // Birds of Prey (And the Fantabulous Emancipation of One Harley Quinn)
-    if (formattedTitle.charAt(formattedTitle.length - 1) === '-') {
+    if (formattedTitle.charAt(formattedTitle.length - 1) === '_') {
       return formattedTitle.slice(0, -1);
     } else {
       return formattedTitle;
