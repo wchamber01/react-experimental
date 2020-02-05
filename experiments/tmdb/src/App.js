955efe337ea9a9fdaa7880aeab91ff513086a765
samsisle <samko9522@gmail.com>
2020-02-04 22:48:45 -0800

    Upgrade routes config to react-router-dom v6

--- experiments/tmdb/src/App.js
+++ experiments/tmdb/src/App.js
@@ -1,19 +1,17 @@
 import React, { Suspense } from 'react';
-import { Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
-import NowPlaying from './pages/Home';
+import Home from './pages/Home';
 import Movie from './pages/Movie';
 import Spinner from './components/Spinner';
 
 export default function App() {
   return (
     <Suspense fallback={<Spinner />}>
-      <Route exact path="/">
-        <NowPlaying />
-      </Route>
-      <Route path="/:movieTitle">
-        <Movie />
-      </Route>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/:movieTitle" element={<Movie />} />
+      </Routes>
     </Suspense>
   );
 }
