9333b05fababe9e88514ef2ab072bf84f3f24e19
samsisle <samko9522@gmail.com>
2020-02-06 00:03:45 -0800

    Update Backdrop component

--- experiments/tmdb/src/pages/Movie/Backdrop.js
+++ experiments/tmdb/src/pages/Movie/Backdrop.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import back from '../../lib/arrow-left-bold.svg';
 
 import css from './Backdrop.module.css';
 
 export default function Backdrop(props) {
+  const navigate = useNavigate();
+
+  function navigateToHomePage() {
+    navigate('/');
+  }
+
   return (
     <section className={css.backdrop}>
-      <img src={back} alt="go back to Home page" />
+      <img src={back} alt="go back to Home page" onClick={navigateToHomePage} />
       <img
         src={`https://image.tmdb.org/t/p/original${props.backdropPath}`}
         alt={props.title}
