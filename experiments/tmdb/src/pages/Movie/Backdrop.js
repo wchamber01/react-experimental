4e63d50ab4ea288f517ca3fa5594ca10889d25fd
samsisle <samko9522@gmail.com>
2020-02-06 14:28:39 -0800

    Remove unecessary back button

--- experiments/tmdb/src/pages/Movie/Backdrop.js
+++ experiments/tmdb/src/pages/Movie/Backdrop.js
@@ -1,20 +1,10 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
-
-import back from '../../lib/arrow-left-bold.svg';
 
 import css from './Backdrop.module.css';
 
 export default function Backdrop(props) {
-  const navigate = useNavigate();
-
-  function navigateToHomePage() {
-    navigate('/');
-  }
-
   return (
     <section className={css.backdrop}>
-      <img src={back} alt="go back to Home page" onClick={navigateToHomePage} />
       <img
         src={`https://image.tmdb.org/t/p/original${props.backdropPath}`}
         alt={props.title}
