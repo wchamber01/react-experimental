688aa151e15eb25ae7d7899e9de1ff6c4fbdedd4
samsisle <samko9522@gmail.com>
2020-02-08 00:02:34 -0800

    Add Clips component

---
+++ experiments/tmdb/src/pages/Movie/Clips.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import useSwr from 'swr';
+
+import css from './Clips.module.css';
+
+export default function Clips() {
+  return (
+    <section className={css.clips}>
+      <h3>Clips</h3>
+    </section>
+  );
+}
