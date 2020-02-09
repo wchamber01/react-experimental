bb4653ae6e23c0893c249b3395809ccb97abeb1a
samsisle <samko9522@gmail.com>
2020-02-09 15:48:46 -0800

    Add convertToOrdinal function

--- experiments/tmdb/src/lib/calendar.js
+++ experiments/tmdb/src/lib/calendar.js
@@ -13,4 +13,9 @@ export const months = [
   'December'
 ];
 
-export const ordinals = ['th', 'st', 'nd', 'rd'];
+export function convertToOrdinal(str) {
+  const num = Number(str); // convert String to Number primitive
+  const oS = ['th', 'st', 'nd', 'rd']; // ordinal suffixes
+
+  return `${str}${oS[(num - 20) % 10] || oS[num] || oS[0]}`;
+}
