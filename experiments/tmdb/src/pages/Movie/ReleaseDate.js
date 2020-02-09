04034ad7d79766f5811c774adaf54c01f70d8277
samsisle <samko9522@gmail.com>
2020-02-09 15:49:04 -0800

    Update ReleaseDate component

--- experiments/tmdb/src/pages/Movie/ReleaseDate.js
+++ experiments/tmdb/src/pages/Movie/ReleaseDate.js
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 import useSwr from 'swr';
 
 import { MovieContext } from './MovieContext';
-import { months, ordinals } from '../../lib/calendar';
+import { months, convertToOrdinal } from '../../lib/calendar';
 
 import css from './Info.module.css';
 
@@ -18,23 +18,15 @@ export default function ReleaseDate() {
 
   function releaseDateConversion(dates) {
     const { release_dates } = dates.find(date => date.iso_3166_1 === 'US');
-    const official_release = release_dates.find(date => date.note === '');
+    const official_release = release_dates.find(date =>
+      ['', 'North American release'].includes(date.note)
+    );
+
     const t = official_release.release_date.slice(0, 10);
 
     const year = t.slice(0, 4);
     const month = months[Number(t.slice(5, 7)) - 1];
-
-    let day;
-    let dayNumber = Number(t.slice(-2));
-    let daySinglesDigit = Number(t.slice(-1));
-
-    const ordinal = daySinglesDigit > 3 ? 'th' : ordinals[daySinglesDigit];
-
-    if (dayNumber > 9) {
-      day = dayNumber.toString() + ordinal;
-    } else {
-      day = daySinglesDigit.toString() + ordinal;
-    }
+    const day = convertToOrdinal(t.slice(-2));
 
     return `${month} ${day}, ${year}`;
   }
