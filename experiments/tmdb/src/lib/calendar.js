d9476b8ff147dda1822affbe69e35776c58409c6
samsisle <samko9522@gmail.com>
2020-02-10 16:52:44 -0800

    Update convertToOrdinal function

--- experiments/tmdb/src/lib/calendar.js
+++ experiments/tmdb/src/lib/calendar.js
@@ -17,5 +17,5 @@ export function convertToOrdinal(str) {
   const num = Number(str); // convert String to Number primitive
   const oS = ['th', 'st', 'nd', 'rd']; // ordinal suffixes
 
-  return `${str}${oS[(num - 20) % 10] || oS[num] || oS[0]}`;
+  return `${num}${oS[(num - 20) % 10] || oS[num] || oS[0]}`;
 }
