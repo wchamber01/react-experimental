9a2cfa3b0e87e0c059820a259caf55593e9e5a50
samsisle <samko9522@gmail.com>
2020-02-14 20:43:34 -0800

    Update ReleaseDate component

--- experiments/tmdb/src/pages/Movie/ReleaseDate.js
+++ experiments/tmdb/src/pages/Movie/ReleaseDate.js
@@ -1,30 +1,12 @@
-import React, { useContext } from 'react';
-import useSwr from 'swr';
+import React from 'react';
 
-import { MovieContext } from './MovieContext';
 import { months, convertToOrdinal } from '../../lib/calendar';
 
 import css from './css/Info.module.css';
 
-export default function ReleaseDate() {
-  const { movieId } = useContext(MovieContext);
-
-  const { data: releases } = useSwr(
-    `https://api.themoviedb.org/3/movie/${movieId}/release_dates`,
-    {
-      suspense: true
-    }
-  );
-
-  function releaseDateConversion(dates) {
-    const { release_dates } = dates.find(date => date.iso_3166_1 === 'US');
-    const official_release = release_dates.find(date =>
-      ['', 'North American release', 'New York and Los Angeles'].includes(
-        date.note
-      )
-    );
-
-    const t = official_release.release_date.slice(0, 10);
+export default function ReleaseDate(props) {
+  function releaseDateConversion(releaseDate) {
+    const t = releaseDate.slice(0, 10);
 
     const year = t.slice(0, 4);
     const month = months[Number(t.slice(5, 7)) - 1];
@@ -36,7 +18,7 @@ export default function ReleaseDate() {
   return (
     <li className={css.info}>
       <span className={css.credits}>Release</span>
-      {releaseDateConversion(releases.results)}
+      {releaseDateConversion(props.releaseDate)}
     </li>
   );
 }
