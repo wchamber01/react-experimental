244f85bebf70833de271b0ea350c6d5c8247c0c5
samsisle <samko9522@gmail.com>
2020-02-14 16:25:19 -0800

    Update Home page

--- experiments/tmdb/src/pages/Home/Home.js
+++ experiments/tmdb/src/pages/Home/Home.js
@@ -9,10 +9,10 @@ export default function Home() {
   return (
     <>
       <div className={css.dynamicPaddingTop}></div>
-      <div className={css.container}>
+      <main className={css.container}>
         <NowPlaying />
         <Upcoming />
-      </div>
+      </main>
     </>
   );
 }
