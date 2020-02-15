c094e4fec9e4a4b4c1fa9526c56982aeea900f17
samsisle <samko9522@gmail.com>
2020-02-14 16:12:36 -0800

    Import Footer component into App component

--- experiments/tmdb/src/App.js
+++ experiments/tmdb/src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 
 import { Home, Movie } from './pages';
 import { Spinner } from './components/Spinner';
+import { Footer } from './components/Footer';
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
         <Route exact path="/" element={<Home />} />
         <Route path=":movieId/*" element={<Movie />} />
       </Routes>
+      <Footer />
     </Suspense>
   );
 }
