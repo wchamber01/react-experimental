7aeb58cb841dcad07a64ec8a142d1c3d6fb74bba
samsisle <samko9522@gmail.com>
2020-02-16 16:15:31 -0800

    Add releaseTypes.js

---
+++ experiments/tmdb/src/lib/releaseTypes.js
@@ -0,0 +1,12 @@
+/**
+ * A growing array of acceptable North American release types.
+ * An empty string is the most common release type that comes
+ * back from the TMDb database.
+ */
+
+export const releaseTypes = [
+  '',
+  'North American release',
+  'New York and Los Angeles',
+  'select theaters'
+];
